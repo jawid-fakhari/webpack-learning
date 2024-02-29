d912ad25c757c500c383efb99a772467440ca9c5
Jawid Fakhari <jawid_fakhari@live.it>
2024-02-29 14:00:29 +0100

    css loaader

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,10 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.css$/,
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }],
+      },
     ],
   },
 };
